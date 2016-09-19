3bd05cf79907233d7f6696899024042ac7cdcfaa
昊 <zh179092007@gmail.com>
2016-09-20 00:38:01 +0800

    Redis操作模板

--- src/main/java/org/menina/template/RedisTemplate.java
+++ src/main/java/org/menina/template/RedisTemplate.java
@@ -1,10 +1,17 @@
 package org.menina.template;
 
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by meninaChimp on 2016/9/18 0018.
  */
+@Component
 public class RedisTemplate {
 
     private JedisPool jedispool;
@@ -13,5 +20,25 @@ public class RedisTemplate {
         this.jedispool = jedispool;
     }
 
+    public <T extends Serializable> T redisInvoke(String methodName, Object... args) {
+        Jedis jedis = jedispool.getResource();
+        try{
+            Class[] argsType = new Class[args.length];
+            for(int i = 0; i< args.length; i++){
+                argsType[i] = args[i].getClass();
+            }
 
+            Method method = jedis.getClass().getDeclaredMethod(methodName, argsType);
+            return (T)method.invoke(jedis, args);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }finally {
+            jedis.close();
+            return null;
+        }
+    }
 }
