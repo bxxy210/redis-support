455ae667743780fe087d5ff31475a1f4622f6349
昊 <zh179092007@gmail.com>
2016-09-30 00:12:57 +0800

    实现String操作

--- src/main/java/org/menina/AbstractRedisSupport.java
+++ src/main/java/org/menina/AbstractRedisSupport.java
@@ -2,6 +2,7 @@ package org.menina;
 
 import lombok.extern.slf4j.Slf4j;
 import org.menina.template.RedisTemplate;
+import org.menina.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -14,10 +15,45 @@ public abstract class AbstractRedisSupport implements RedisSupport {
     private RedisTemplate redisTemplate;
 
     public String get(String key) {
-        if(key.equals("")){
-            return "";
+        if(StringUtils.isEmpty(key)){
+            return key;
         }
 
-        return redisTemplate.redisInvoke("get", key);
+        String result = redisTemplate.redisInvoke("get", key);
+        return result;
+    }
+
+    @Override
+    public void set(String key, String value) {
+        redisTemplate.redisInvoke("set", key, value);
+    }
+
+    @Override
+    public void expireAt(String key, long unixTimeSeconds) {
+        if(unixTimeSeconds*1000 <= System.currentTimeMillis()){
+            return;
+        }
+
+        redisTemplate.redisInvoke("expireAt", key, unixTimeSeconds);
+    }
+
+    @Override
+    public Long expire(String key, int seconds) {
+        return redisTemplate.redisInvoke("expire", key, seconds);
+    }
+
+    @Override
+    public Long ttl(String key) {
+        return redisTemplate.redisInvoke("ttl", "key");
+    }
+
+    @Override
+    public Long del(String key) {
+        return redisTemplate.redisInvoke("del", "key");
+    }
+
+    @Override
+    public void persist(String key) {
+        redisTemplate.redisInvoke("persist", "key");
     }
 }
--- src/main/java/org/menina/RedisSupport.java
+++ src/main/java/org/menina/RedisSupport.java
@@ -4,5 +4,56 @@ package org.menina;
  * Created by meninaChimp on 2016/9/19 0019.
  */
 public interface RedisSupport {
+
+    /**
+     * 获取一个值
+     * @param key
+     * @return
+     */
     String get(String key);
+
+    /**
+     * 存一个键值对
+     * @param key
+     * @param value
+     */
+    void set(String key, String value);
+
+    /**
+     * 指定键在unix时间超时过期
+     * @param key
+     * @param unixTimeSeconds
+     */
+    void expireAt(String key, long unixTimeSeconds);
+
+    /**
+     * 指定键在传入秒数后超时过期
+     * @param key
+     * @param seconds
+     * @return 1 设置成功
+     *          0 设置失败
+     */
+    Long expire(String key, int seconds);
+
+    /**
+     * 键剩余存活毫秒数
+     * @param key
+     * @return
+     */
+    Long ttl(String key);
+
+    /**
+     * 删除一个键值对
+     * @param key
+     * @return
+     */
+    Long del(String key);
+
+    /**
+     * 持久化，移除键值对的过期时间
+     * @param key
+     * @return
+     */
+    void persist(String key);
+
 }
