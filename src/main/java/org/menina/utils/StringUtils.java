8fd664fdcdfa2caea73fad11a293f5a63b119ea5
昊 <zh179092007@gmail.com>
2016-09-30 00:12:12 +0800

    字符串工具类

---
+++ src/main/java/org/menina/utils/StringUtils.java
@@ -0,0 +1,15 @@
+package org.menina.utils;
+
+/**
+ * Created by meninaChimp on 2016/9/29 0029.
+ */
+public class StringUtils {
+
+    public static boolean isEmpty(String str){
+        if("".equals(str)){
+            return true;
+        }
+
+        return false;
+    }
+}
